033a4d43dfa2d8d4fc059cff01a00e18e804e88d
Mingaleev-D92 <mingaleev,d92@mail.ru>
2022-12-13 22:40:45 +0300

    ошибка с библиотекой(устранение).Навигация

--- app/src/main/java/com/example/fakeecommece/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/fakeecommece/ui/activities/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.example.fakeecommece
+package com.example.fakeecommece.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fakeecommece.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
