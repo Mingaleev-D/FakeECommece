bd62066673443c69adf763ffc4321da829a4df0f
Mingaleev-D92 <mingaleev,d92@mail.ru>
2022-12-14 13:28:49 +0300

    логин фрагмент и навигация

--- app/src/main/java/com/example/fakeecommece/ui/fragments/IntroductionFragment.kt
+++ app/src/main/java/com/example/fakeecommece/ui/fragments/IntroductionFragment.kt
@@ -1,21 +1,38 @@
 package com.example.fakeecommece.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.fakeecommece.R
+import com.example.fakeecommece.databinding.FragmentIntroductionBinding
 
 
 class IntroductionFragment : Fragment() {
+
+   private var mBinding: FragmentIntroductionBinding? = null
+   private val binding get() = mBinding!!
 
    override fun onCreateView(
       inflater: LayoutInflater, container: ViewGroup?,
       savedInstanceState: Bundle?
    ): View? {
-      // Inflate the layout for this fragment
-      return inflater.inflate(R.layout.fragment_introduction, container, false)
+      mBinding = FragmentIntroductionBinding.inflate(inflater, container, false)
+      return binding.root
+   }
+
+   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+      super.onViewCreated(view, savedInstanceState)
+      binding.btnStart.setOnClickListener {
+         findNavController().navigate(R.id.action_introductionFragment_to_accountOptionsFragment)
+      }
+   }
+
+   override fun onDestroyView() {
+      super.onDestroyView()
+      mBinding = null
    }
 
 
--- app/src/main/java/com/example/fakeecommece/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/fakeecommece/ui/fragments/LoginFragment.kt
@@ -1,60 +1,80 @@
 package com.example.fakeecommece.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.example.fakeecommece.R
+import com.example.fakeecommece.databinding.FragmentLoginBinding
+import com.example.fakeecommece.ui.activities.ShoppingActivity
+import com.example.fakeecommece.ui.util.Resource
+import com.example.fakeecommece.ui.viewmodel.LoginViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+@AndroidEntryPoint
 class LoginFragment : Fragment() {
-   // TODO: Rename and change types of parameters
-   private var param1: String? = null
-   private var param2: String? = null
 
-   override fun onCreate(savedInstanceState: Bundle?) {
-      super.onCreate(savedInstanceState)
-      arguments?.let {
-         param1 = it.getString(ARG_PARAM1)
-         param2 = it.getString(ARG_PARAM2)
-      }
-   }
+   private var mBinding: FragmentLoginBinding? = null
+   private val binding get() = mBinding!!
+   private val viewModel by viewModels<LoginViewModel>()
 
    override fun onCreateView(
       inflater: LayoutInflater, container: ViewGroup?,
       savedInstanceState: Bundle?
-   ): View? {
-      // Inflate the layout for this fragment
-      return inflater.inflate(R.layout.fragment_login, container, false)
+   ): View {
+      mBinding = FragmentLoginBinding.inflate(inflater, container, false)
+      return binding.root
    }
 
-   companion object {
-      /**
-       * Use this factory method to create a new instance of
-       * this fragment using the provided parameters.
-       *
-       * @param param1 Parameter 1.
-       * @param param2 Parameter 2.
-       * @return A new instance of fragment LoginFragment.
-       */
-      // TODO: Rename and change types and number of parameters
-      @JvmStatic
-      fun newInstance(param1: String, param2: String) =
-         LoginFragment().apply {
-            arguments = Bundle().apply {
-               putString(ARG_PARAM1, param1)
-               putString(ARG_PARAM2, param2)
+   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+      super.onViewCreated(view, savedInstanceState)
+
+      binding.tvDontHaveAccount.setOnClickListener {
+         findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+      }
+
+      binding.apply {
+         btnLogin.setOnClickListener {
+            val email = edEmailLogin.text.toString().trim()
+            val password = edPasswordLogin.text.toString()
+            viewModel.login(email, password)
+         }
+      }
+
+      lifecycleScope.launchWhenStarted {
+         viewModel.login.collect {
+            when (it) {
+               is Resource.Loading -> {
+                  binding.btnLogin.startAnimation()
+               }
+               is Resource.Success -> {
+                  binding.btnLogin.revertAnimation()
+                  Intent(requireContext(), ShoppingActivity::class.java).also { intent ->
+                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                     startActivity(intent)
+                  }
+               }
+               is Resource.Error   -> {
+                  Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
+                  binding.btnLogin.revertAnimation()
+               }
+               else                -> Unit
             }
          }
+      }
    }
+
+   override fun onDestroyView() {
+      super.onDestroyView()
+      mBinding = null
+   }
+
+
 }
